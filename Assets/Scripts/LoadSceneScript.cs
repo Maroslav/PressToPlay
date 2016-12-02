7e7bf4faada722bc59f3746cfe6a0b3c1551d91d
Martin Milota <milotov@seznam.cz>
2016-12-02 12:44:02 +0100

    + Added the LoadSceneScript + Added the Play button prefab

---
+++ Assets/Scripts/LoadSceneScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class LoadSceneScript : MonoBehaviour
+    {
+        public string SceneName;
+
+        public void LoadScene()
+        {
+            Debug.Log("Loading scene: " + SceneName);
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
