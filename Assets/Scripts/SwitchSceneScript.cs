f8ffeb5039cd7b9af806bbb9c616c2e931444463
Martin Milota <milotov@seznam.cz>
2016-12-02 13:07:56 +0100

    * modified switch scene button prefab * improvements to menu scene

---
+++ Assets/Scripts/SwitchSceneScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class SwitchSceneScript
+            : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            Debug.Log("Loading scene: " + sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void ExitGame()
+        {
+            Debug.Log("Exiting...");
+            Application.Quit();
+        }
+    }
+}
