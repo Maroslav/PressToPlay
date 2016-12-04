0204ae747af30dcba4a16025b52f5cb5ebdb3b51
Martin Milota <milotov@seznam.cz>
2016-12-04 11:41:47 +0100

    * Framerate limited to 60

---
+++ Assets/Scripts/FrameRateLimiter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameRateLimiter : MonoBehaviour
+{
+    public int FrameRate = 60;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(ChangeFramerate());
+    }
+
+    IEnumerator ChangeFramerate()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log(string.Format("Setting frame rate cap to {0} fps.", FrameRate));
+        Application.targetFrameRate = FrameRate;
+    }
+}
