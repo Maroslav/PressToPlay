6e82e6b8b8f121fc24fa785a68e3432bca2397a0
Martin Milota <milotov@seznam.cz>
2016-12-04 18:06:34 +0100

    + Added the world state provider (game object that manages the world state)

---
+++ Assets/Scripts/WorldStateProvider.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Code.GameState;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class WorldStateProvider : MonoBehaviour
+{
+    public static WorldState State { get; private set; }
+
+
+    void Start()
+    {
+        Assert.IsNull(State);
+        State = new WorldState();
+        // TODO: Different creation/deserialization
+    }
+}
