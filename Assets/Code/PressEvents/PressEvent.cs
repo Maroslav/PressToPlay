f2edec760c9f064f77188765a3ac82a4f499baeb
Martin Milota <milotov@seznam.cz>
2016-12-04 18:13:05 +0100

    * Organized code

--- Assets/Code/PressEvents/PressEvent.cs
+++ Assets/Code/PressEvents/PressEvent.cs
@@ -4,14 +4,14 @@ namespace Assets.Code.PressEvents
 {
     public abstract class PressEvent
     {
+        public DateTime Date { get; private set; }
+        public bool IsFinished { get;protected set; }
+
         protected PressEvent(DateTime date)
         {
             Date = date;
             IsFinished = false;
         }
-
-        public DateTime Date { get; private set; }
-        public bool IsFinished { get;protected set; }
 
         //Visitor pattern, corresponds to the 'accept' method.
         public abstract void ProcessEvent(IEventProcessor processor);
