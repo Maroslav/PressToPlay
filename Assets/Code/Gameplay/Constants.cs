c356fd62ac366bda0d7f6f3966bfde73c4ffa35f
Formicarufa <Formicarufa@users.noreply.github.com>
2016-12-09 10:35:16 +0100

    Game constants file

---
+++ Assets/Code/Gameplay/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Code.Gameplay
+{
+    public static class Constants
+    {
+        public const int DefaultChoicesCount = 2;
+        public const string RandomEventsScenarioLoc = "Assets/Scenarios/random.xml";
+        public const string StoryEventsScenarioLoc = "Assets/Scenarios/story.xml";
+    }
+}
