74d8f082d58a835f571f8baf107b225019ce3c0e
Martin Milota <milotov@seznam.cz>
2016-12-23 16:23:06 +0100

    * Added tweening of the popup text with the slider tween as a continuation

--- Assets/Scripts/World/WorldAttributeChanger.cs
+++ Assets/Scripts/World/WorldAttributeChanger.cs
@@ -1,14 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 public class WorldAttributeChanger : MonoBehaviour
 {
+    // The undisputed value of the attribute that this script represents
+    public float AttributeValue;
+
     public GameObject DescriptionGameObject;
     public GameObject SliderGameObject;
     public GameObject PopupGameObject;
-
-    public float AttributeValue;
 
 
     private void OnValidate()
@@ -17,30 +19,45 @@ public class WorldAttributeChanger : MonoBehaviour
         Assert.IsNotNull(DescriptionGameObject.GetComponent<Text>(), name);
         Assert.IsNotNull(SliderGameObject, name);
         Assert.IsNotNull(SliderGameObject.GetComponent<Slider>(), name);
-        Assert.IsNotNull(SliderGameObject.GetComponent<SimpleTweener>(), name);
+        Assert.IsNotNull(SliderGameObject.GetComponent<FloatTweener>(), name);
         Assert.IsNotNull(PopupGameObject, name);
         Assert.IsNotNull(PopupGameObject.GetComponent<Text>(), name);
+        Assert.IsNotNull(PopupGameObject.GetComponent<Vector3Tweener>(), name);
+    }
+
+    void Start()
+    {
+        PopupGameObject.SetActive(false);
     }
 
 
     public void DoSetup(string description, int minValue, int maxValue)
     {
+        // Desc
         Text desc = DescriptionGameObject.GetComponent<Text>();
         desc.text = description;
 
+        // Slider
         Slider s = SliderGameObject.GetComponent<Slider>();
         s.minValue = minValue;
         s.maxValue = maxValue;
 
-        var sliderTweener = SliderGameObject.GetComponent<SimpleTweener>();
+        var sliderTweener = SliderGameObject.GetComponent<FloatTweener>();
         sliderTweener.Init(
             (minValue + maxValue) >> 1,
-            val => s.value = val);
-
+            val => s.value = val, // 3. keep changing slider value
+            abrupt => PopupGameObject.SetActive(false)); // 4. hide the popup
     }
 
     public void DoChange(int value, bool fancyAnims = false)
     {
+        float diff = value - AttributeValue;
+
+        // Update popup
+        var popupText = PopupGameObject.GetComponent<Text>();
+        popupText.text = string.Format("{0}{1}", diff > 0 ? "+" : string.Empty, diff);
+
+        // Save the new value AFTER
         AttributeValue = value;
 
         if (!fancyAnims)
@@ -50,10 +67,22 @@ public class WorldAttributeChanger : MonoBehaviour
             return;
         }
 
-        var tweener = SliderGameObject.GetComponent<SimpleTweener>();
-        tweener.TweenTo(value);
+        if (Math.Abs(diff) < 0.001f)
+            return;
 
-        //PopupGameObject.SetActive(true);
-        //PopupGameObject.SetActive(false);
+        // Start the tweening action
+        PopupGameObject.SetActive(true);
+        PopupGameObject.transform.localScale = Vector3.one;
+
+        var popupTweener = PopupGameObject.GetComponent<Vector3Tweener>();
+        var sliderTweener = SliderGameObject.GetComponent<FloatTweener>();
+
+        // Re-init the tweener to update the finish function (it takes this method's parameter as argument)
+        popupTweener.Init(
+            PopupGameObject.transform.localScale,
+            val => PopupGameObject.transform.localScale = val, // 1. keep chaning popup scale
+            abrupt => sliderTweener.TweenTo(value)); // 2. start slider tween
+
+        popupTweener.TweenTo(new Vector3(2, 2, 2));
     }
 }
