01dfe54616611fdb4af10cf19114f17f713c209f
Martin Milota <milotov@seznam.cz>
2016-12-23 15:25:47 +0100

    * Reworked the WorldAttribute to use the SimpleTweener script

--- Assets/Scripts/World/WorldAttributeChanger.cs
+++ Assets/Scripts/World/WorldAttributeChanger.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 public class WorldAttributeChanger : MonoBehaviour
 {
     public GameObject DescriptionGameObject;
-
     public GameObject SliderGameObject;
-    public float SlideAnimationSpeedCoef = 1;
-
     public GameObject PopupGameObject;
-    private const float PopupAnimationLenght = 3.5f;
-    public float PopupAnimationSpeedCoef = 1;
+
+    public float AttributeValue;
 
 
     private void OnValidate()
@@ -22,6 +17,7 @@ public class WorldAttributeChanger : MonoBehaviour
         Assert.IsNotNull(DescriptionGameObject.GetComponent<Text>(), name);
         Assert.IsNotNull(SliderGameObject, name);
         Assert.IsNotNull(SliderGameObject.GetComponent<Slider>(), name);
+        Assert.IsNotNull(SliderGameObject.GetComponent<SimpleTweener>(), name);
         Assert.IsNotNull(PopupGameObject, name);
         Assert.IsNotNull(PopupGameObject.GetComponent<Text>(), name);
     }
@@ -35,69 +31,32 @@ public class WorldAttributeChanger : MonoBehaviour
         Slider s = SliderGameObject.GetComponent<Slider>();
         s.minValue = minValue;
         s.maxValue = maxValue;
+
+        var sliderTweener = SliderGameObject.GetComponent<SimpleTweener>();
+        sliderTweener.Init(
+            (minValue + maxValue) >> 1,
+            val => s.value = val,
+            () => Debug.Log("Finished tween."));
+
     }
 
     public void DoChange(int value, bool fancyAnims = false)
     {
-        if (!fancyAnims)
-        {
-            var s = SliderGameObject.GetComponent<Slider>();
-            s.value = value;
-            return;
-        }
+        AttributeValue = value;
 
+        //if (!fancyAnims)
+        //{
+        //    var s = SliderGameObject.GetComponent<Slider>();
+        //    s.value = value;
+        //    Debug.Log("Hard set to: " + value);
+        //    return;
+        //}
 
+        Debug.Log("Tween to: " + value);
+        var tweener = SliderGameObject.GetComponent<SimpleTweener>();
+        tweener.DoStart(value);
 
-
-
-        // TODO: log time !!!
-
-
-
-        var popupHash = iTween.Hash(
-            "name", "AttributePopupTween_" + gameObject.name,
-            "from", Vector3.one * 2,
-            "to", Vector3.one,
-            "time", PopupAnimationLenght * PopupAnimationSpeedCoef,
-            "easetype", iTween.EaseType.easeOutElastic,
-            "onupdate", "OnPopupTweenUpdate",
-            "oncomplete", "OnPopupTweenComplete",
-            "oncompleteparams", value);
-        iTween.ValueTo(gameObject, popupHash);
-        PopupGameObject.SetActive(true);
-    }
-
-    void OnPopupTweenUpdate(Vector3 newValue)
-    {
-        var p = PopupGameObject.GetComponent<Text>();
-        p.transform.localScale = newValue;
-    }
-
-    void OnPopupTweenComplete(float newValue)
-    {
-        Debug.Log("Popup complete");
-        Slider s = SliderGameObject.GetComponent<Slider>();
-
-        var sliderHash = iTween.Hash(
-            "name", "AttributeSliderTween_" + gameObject.name,
-            "from", s.value,
-            "to", newValue,
-            "time", 10 * (newValue - s.value) / s.maxValue * SlideAnimationSpeedCoef,
-            "easetype", iTween.EaseType.easeOutBack,
-            "onupdate", "OnSliderTweenUpdate",
-            "oncomplete", "OnSliderTweenComplete");
-        iTween.ValueTo(gameObject, sliderHash);
-    }
-
-    void OnSliderTweenUpdate(float newValue)
-    {
-        var s = SliderGameObject.GetComponent<Slider>();
-        s.value = newValue;
-    }
-
-    void OnSliderTweenComplete()
-    {
-        Debug.Log("Slide complete");
-        PopupGameObject.SetActive(false);
+        //PopupGameObject.SetActive(true);
+        //PopupGameObject.SetActive(false);
     }
 }
