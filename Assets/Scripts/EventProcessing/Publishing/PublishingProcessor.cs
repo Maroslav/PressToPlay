d0bad005c47f7b93be2ef92b5a7fc0e8c4612ed9
Martin Milota <milotov@seznam.cz>
2017-01-06 16:30:54 +0100

    + Added a preliminary publishing script

---
+++ Assets/Scripts/EventProcessing/Publishing/PublishingProcessor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Assets.Code.PressEvents;
+using Assets.Code.PressEvents.Choices;
+
+public class PublishingProcessor : MonoBehaviour
+{
+    public GameObject Title;
+
+
+    private PressEvent _event;
+
+
+    //void OnValidate()
+    //{
+    //    Assert.IsNotNull(ChoiceDescription, name);
+    //    Assert.IsNotNull(ChoiceDescription.GetComponent<Text>(), name);
+
+    //    Assert.IsNotNull(ChoiceDate, name);
+    //    Assert.IsNotNull(ChoiceDate.GetComponent<Text>(), name);
+
+    //    Assert.IsNotNull(ChoiceViewer, name);
+    //    Assert.IsNotNull(ChoiceViewer.GetComponent<MultipleChoiceProcessor>(), name);
+
+    //    Assert.IsNotNull(Publishing, name);
+    //    Assert.IsNotNull(Publishing.GetComponent<PublishingProcessor>(), name);
+    //}
+
+
+    public void Publish(PressEvent e, TextChoice choice)
+    {
+        _event = e;
+
+        //    ChoiceDescription.GetComponent<Text>().text = choice.Description;
+        //    MultipleChoiceData multipleChoiceData = ChoicePublish.GetComponent<MultipleChoiceData>();
+        //    multipleChoiceData.Choice = choice;
+        //    multipleChoiceData.SetEvent(_event, this);
+    }
+
+    public void Finish()
+    {
+    }
+}
