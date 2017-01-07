0a571c475748344d7cb0bb97178b677d5086e2b8
Martin Bulant <bulisekspam@centrum.cz>
2017-01-07 12:06:58 +0100

    Images in choice buttons require acpect ratio 1,77:1 which is used to create margin.

---
+++ Assets/Scripts/EventProcessing/ImageSelectionEvent/ImageInnerButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class ImageInnerButton : MonoBehaviour
+{
+
+    public GameObject ButtonContainer;
+
+    void OnValidate()
+    {
+        Assert.IsNotNull(ButtonContainer);
+        Assert.IsNotNull(ButtonContainer.GetComponent<ImageChoiceData>());
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("ImageInnerButton.OnClick");
+        ButtonContainer.GetComponent<ImageChoiceData>().OnClick();
+    }
+}
