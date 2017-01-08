c735f4e14079212e2596b4215192ddff713c7ab5
Martin Milota <milotov@seznam.cz>
2017-01-08 23:06:20 +0100

    + Added a base class for choice event processors * Moved Description from Text/Image events to Choice event

--- Assets/Scripts/EventProcessing/MultipleChoiceEvent/MultipleChoiceEventProcessor.cs
+++ Assets/Scripts/EventProcessing/MultipleChoiceEvent/MultipleChoiceEventProcessor.cs
@@ -1,68 +1,33 @@
 ﻿using UnityEngine;
-using System.Globalization;
 using Assets.Code.PressEvents;
 using Assets.Code.PressEvents.Choices;
 using UnityEngine.Assertions;
-using UnityEngine.UI;
 
-public class MultipleChoiceEventProcessor : MonoBehaviour
+public class MultipleChoiceEventProcessor : ChoiceEventProcessorBase<MultipleChoiceEvent, TextChoice>
 {
-    public GameObject ChoiceName;
-    public GameObject ChoiceDescription;
-    public GameObject ChoiceDate;
     public GameObject ChoiceViewer;
-
-    public GameObject Publishing;
-
-    private MultipleChoiceEvent _event;
 
 
     void OnValidate()
     {
-        Assert.IsNotNull(ChoiceName, name);
-        Assert.IsNotNull(ChoiceName.GetComponent<Text>(), name);
-
-        Assert.IsNotNull(ChoiceDescription, name);
-        Assert.IsNotNull(ChoiceDescription.GetComponent<Text>(), name);
-
-        Assert.IsNotNull(ChoiceDate, name);
-        Assert.IsNotNull(ChoiceDate.GetComponent<Text>(), name);
-
         Assert.IsNotNull(ChoiceViewer, name);
         Assert.IsNotNull(ChoiceViewer.GetComponent<MultipleChoiceProcessor>(), name);
-
-        Assert.IsNotNull(Publishing, name);
-        Assert.IsNotNull(Publishing.GetComponent<PublishingProcessor>(), name);
     }
 
 
-    public void ProcessEvent(MultipleChoiceEvent e)
+    public override void ProcessEvent(MultipleChoiceEvent e)
     {
-        _event = e;
-        Debug.Log("Processing new multiple choice event: " + _event.Date);
-        gameObject.SetActive(true);
-
-        // Set event description in the UI
-        ChoiceName.GetComponent<Text>().text = e.Name;
-        ChoiceDescription.GetComponent<Text>().text = e.Description;
-        ChoiceDate.GetComponent<Text>().text = e.Date.ToString("d", new CultureInfo("cs-CZ"));
+        base.ProcessEvent(e);
 
         // Let the viewer set its content
         ChoiceViewer.GetComponent<MultipleChoiceProcessor>().ProcessEvent(e, this);
     }
 
-    internal void ProcessChoice(TextChoice choice)
+    internal override void ProcessChoice(TextChoice choice)
     {
-        Debug.Log("Choosing option " + choice.Title);
+        base.ProcessChoice(choice);
 
-        // Remove old choices and hide not neccesary things
+        // Remove old choices
         ChoiceViewer.GetComponent<MultipleChoiceProcessor>().DestroyChoices();
-        gameObject.SetActive(false);
-
-        // Let the event apply the changes to the world state
-        _event.Apply(choice, WorldStateProvider.State);
-        WorldStateProvider.UpdateAttributeGameObjects();
-        // Let Publishing handle the rest (this component is now finished)
-        Publishing.GetComponent<PublishingProcessor>().Publish(_event, choice);
     }
 }
